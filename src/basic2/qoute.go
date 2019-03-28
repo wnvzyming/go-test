d868a5599d461145206b0d6ff7e577e6ac722387
wnvzyming <wnvzyming@sina.com>
2019-03-28 17:50:52 +0800

    push remote

--- src/basic2/qoute.go
+++ src/basic2/qoute.go
@@ -3,12 +3,12 @@ package main
 import "fmt"
 
 func val( a int) int {
-	return a++
+	return a
 }
-
-func  qoute( a *int)  int {
-	return a++
-}
+//
+//func  qoute( a *int)  int {
+//	return a++
+//}
 
 func main()  {
 	a := 3
